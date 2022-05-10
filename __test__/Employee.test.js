b16fd0b7fd18ffcf254273baf27dd32a8b24bec2
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-10 15:13:53 -0400

    passed all employee tests and finished employee constructor

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,7 +1,10 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-    const employee = new Employee('bob');
+    const employee = new Employee('bob', 1, 'bob@email.com');
   
     expect(employee.name).toBe('bob');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
+
