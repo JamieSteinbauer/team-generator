d08a914a7fb78ac8f980299cc90ee68b6f0f8840
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-10 16:51:55 -0400

    created engineer html

--- src/page-template.js
+++ src/page-template.js
@@ -8,6 +8,19 @@ const generateEmployees = (myEmployees) => {
             <p class="card-text">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
             <p class="card-text">Office Number: ${manager.getOfficeNumber()}</p>
             </div>
-        </div>`
+        </div>
+        `;
+    }
+    const generateEngineer = (engineer) => {
+        return `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${engineer.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${engineer.getId()}</h6>
+            <p class="card-text">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+            <p class="card-text">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="no">${engineer.getGithub()}</a></p>
+            </div>
+        </div>
+        `;
     }
 }
