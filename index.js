f0da78cc4e6b122b944e1a4277092375f4714de5
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-26 18:27:00 -0400

    fixed functionality and added comments

--- index.js
+++ index.js
@@ -1,80 +1,96 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateEmployees = require('./lib/generateEmployees');
+const generateEmployees = require('./src/page-template');
 
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
-const saveTeamInfo = [];
+const saveTeamData = [];
 
+// questions for user
 const questions = async () => {
     const answers = await inquirer.prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your ID?'
-    },
-    {
-        type: 'list',
-        name: 'role',
-        message: 'What is your role?',
-        choices: ['Manager', 'Engineer', 'Intern']
-    }])
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID#?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is your role?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+    ])
 
+    // prompts questions for Manager
     if (answers.role === 'Manager') {
         const managerAnswers = await inquirer.prompt([{
             type: 'input',
             name: 'officeNumber',
             message: 'What is your office number?'
-        }])
-        const Manager = new Manager(answers.name, answers.id, answers.email, managerAnswers.officeNumber);
-        saveTeamInfo.push(Manager);
+        }, ])
+        //  creates Manager object
+        const manager = new Manager(answers.name, answers.id, answers.email, managerAnswers.officeNumber);
+        //  pushes Manager object to saveTeamData array
+        saveTeamData.push(manager);
     }
+    // prompts questions for Engineer
     else if (answers.role === 'Engineer') {
         const engineerAnswers = await inquirer.prompt([{
             type: 'input',
             name: 'github',
-            message: 'What is your GitHub username?'   
-        }])
+            message: 'What is your GitHub username?'
+        }, ])
 
-        const Engineer = new Engineer(answers.name, answers.id, answers.email, engineerAnswers.github);
-        saveTeamInfo.push(Engineer);
+        const engineer = new Engineer(answers.name, answers.id, answers.email, engineerAnswers.github);
+
+        saveTeamData.push(engineer);
     }
+    // prompts questions for Intern
     else if (answers.role === 'Intern') {
         const internAnswers = await inquirer.prompt([{
             type: 'input',
             name: 'school',
-            message: 'What school do you go to?'
-        }])
+            message: 'What is your school?'
+        }, ])
 
-        const Intern = new Intern(answers.name, answers.id, answers.email, internAnswers.school);
-        saveTeamInfo.push(Intern);
+        const intern = new Intern(answers.name, answers.id, answers.email, internAnswers.school);
+
+        saveTeamData.push(intern);
     }
-
+    //  prompts questions for next Employee
     const nextEmployee = await inquirer.prompt([{
         type: 'confirm',
         name: 'nextEmployee',
-        message: "Would you like to add another employee?"
+        message: 'Would you like to add another employee?'
     }])
-
+    //  if nextEmployee is true, prompts questions for next Employee
     if (nextEmployee.nextEmployee) {
         return questions();
     }
+    //  if nextEmployee is false, calls generateEmployees function
     else {
         return writeToFile();
     }
 }
-
+//  calls questions function
 questions();
 
 function writeToFile() {
-    fs.writeFile('./dist/index.html', generateEmployees(saveTeamInfo), err => {
+    //  creates html file
+    fs.writeFile('./dist/index.html', generateEmployees(saveTeamData), err => {
         if (err) throw err;
-        console.log('Successfully wrote to file!');
+        console.log('Successfully wrote to index.html');
     })
 }
--- src/page-template.js
+++ src/page-template.js
@@ -1,89 +1,121 @@
+// creates employees
 const generateEmployees = (myEmployees) => {
+    //  creates the manager html
     const generateManager = (manager) => {
+        // template literal to create html for manager
         return `
-        <div class="card" style="width: 18rem;">
+        <div class="col-4 p-5">
+        <div class="card bg-danger text-dark">
         <div class="card-body">
-            <h5 class="card-title">${manager.getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${manager.getId()}</h6>
-            <p class="card-text">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
-            <p class="card-text">Office Number: ${manager.getOfficeNumber()}</p>
-            </div>
+        <h5 class="card-title fs-1">${manager.getName()}</h5>
+        <h6 class="card-subtitle mb-2 fs-2"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h6>
+        <p class="card-text p-1 fs-5">ID: ${manager.getId()}</p>
+        <p class="card-text p-1 fs-5">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+        <p class="card-text p-1 fs-5">Office Number: ${manager.getOfficeNumber()}</p>
         </div>
-        `;
+        </div>
+        </div>
+        `
     }
+    //  creates the engineer html
     const generateEngineer = (engineer) => {
+        // template literal to create html for engineer
         return `
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${engineer.getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${engineer.getId()}</h6>
-            <p class="card-text">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
-            <p class="card-text">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="no">${engineer.getGithub()}</a></p>
-            </div>
-        </div>
-        `;
-    }
-    const generateIntern = (intern) => {
-        return `
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${intern.getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${intern.getId()}</h6>
-            <p class="card-text">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
-            <p class="card-text">School: ${intern.getSchool()}</p>
-            </div>
-        </div>
-        `;
-    }
+    <div class="col-4 p-5">
+    <div class="card bg-warning text-dark">
+    <div class="card-body">
+    <h5 class="card-title fs-1">${engineer.getName()}</h5>
+    <h6 class="card-subtitle mb-2 fs-2"><i class="fas fa-glasses mr-2"></i> ${engineer.getRole()}</h6>
+    <p class="card-text p-1 fs-5">ID: ${engineer.getId()}</p>
+    <p class="card-text p-1 fs-5">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+    <p class="card-text p-1 fs-5">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="no">${engineer.getGithub()}</a></p>
+    </div>
+    </div>
+    </div>
+    `
+    };
 
+    // creates the intern html
+    const generateIntern = (intern) => {
+        // template literal to create html for intern
+        return `
+    <div class="col-4 p-5">
+    <div class="card bg-success text-dark">
+    <div class="card-body">
+    <h5 class="card-title fs-1">${intern.getName()}</h5>
+    <h6 class="card-subtitle mb-2 fs-2"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h6>
+    <p class="card-text p-1 fs-5">ID: ${intern.getId()}</p>
+    <p class="card-text p-1 fs-5">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+    <p class="card-text p-1 fs-5">School: ${intern.getSchool()}</p>
+    </div>
+    </div>
+    </div>
+    `
+    };
+
+    //  an empty array to hold the employees based on their role and inputs from the user to be pushed later
     const html = [];
 
+    //  pushes the manager html to the array
     html.push(
+        // the data from generateEmployees is passed in as an argument
         myEmployees
+        // filters the array to find the manager
         .filter((employee) => employee.getRole() === 'Manager')
+        // maps the array to generate the html for the manager
         .map((manager) => generateManager(manager))
-    )
-
+    );
     html.push(
+        // the data from generateEmployees is passed in as an argument
         myEmployees
+        // filters the array to find the engineer
         .filter((employee) => employee.getRole() === 'Engineer')
+        // maps the array to generate the html for the engineer
         .map((engineer) => generateEngineer(engineer))
+        // joins the html array to create a string
         .join('')
-    )
-
+    );
     html.push(
+        // the data from generateEmployees is passed in as an argument
         myEmployees
+        // filters the array to find the intern
         .filter((employee) => employee.getRole() === 'Intern')
+        // maps the array to generate the html for the intern
         .map((intern) => generateIntern(intern))
+        // joins the html array to create a string
         .join('')
-    )
-
+    );
+    // returns all roles and employees and joins the html array to create a string
     return html.join('');
 }
 
+// exports the generateEmployees function
 module.exports = (myEmployees) => {
     return `
     <!DOCTYPE html>
-        <html lang="en">
-        <head>
+    <html lang="en">
+    
+    <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        
-        <title>Team Profile</title>
-        </head>
-
-        <body>
-        <h1 class="text-center">My Team</h1>
-        <div class="container">
-        ${generateEmployees(myEmployees)}
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+            <script src="https://kit.fontawesome.com/0c923aefe5.js" crossorigin="anonymous"></script>
+            <title>Team Profile Generator</title>
+            </head>
+            
+            <body>
+            <h1 class="text-center bg-primary text-white p-5">My Team</h1>
+            <div class="row">
+            ${generateEmployees(myEmployees)}
+            </div>
+            
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-        </script>
-        </body>
-    `;
+            </script>
+            </body>
+            `
 }
